460237a2756fb619e5438c1c21f95875ce19f097
Meechael <mxn@bk.ru>
2024-06-19 14:14:23 +0300

    часть программы

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+from utils import sorted_operations_status, sorted_operations_date, message
+
+
+def main():
+    with open('operations.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    data = sorted_operations_status(data)
+    data = sorted_operations_date(data)
+
+    for i in range(5):
+        print(message(data[i]))
+        print()
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,46 @@
+def sorted_operations_status(data):
+    """Сортировка операций по статусу EXECUTED"""
+    list_data = []
+    for transaction in data:
+        if transaction and transaction.get('state') == "EXECUTED":
+            list_data.append(transaction)
+    return list_data
+
+def sorted_operations_date(data):
+    """Сортировка последних 5 операций по дате"""
+    return sorted(data, key= lambda x: x['date'], reverse=True)[:5]
+
+def format_date(date):
+    """форматирование даты"""
+    format_date = date[0:10].split(sep='-')
+    return f'{format_date[2]}.{format_date[1]}.{format_date[0]}'
+
+def mask_card_number(card_number: str):
+    """маскировка номера карты в формате XXXX XX** **** XXXX"""
+    return f'{card_number[:4]} {card_number[4: 6]} ** **** {card_number[-4:]}' if card_number.isdigit() and len(card_number) == 16 else ValueError('Номер карты неверный')
+
+def mask_account_number(account_number: str):
+    """маскировка номера счета в формате **XXXX"""
+    return f'**{account_number[:4]}' if account_number.isdigit() and len(account_number) > 4 else ValueError('номер неверный')
+
+def mask_in_message(num):
+    """Скрывает номера в сообщении"""
+    if num is None:
+        return ""
+    num = num.split()
+    if num[0] == "Счет":
+        num_mask = mask_account_number(num[-1])
+    else:
+        num_mask = mask_card_number(num[-1])
+    return ' '.join(num[:-1]) + ' ' + num_mask
+
+def message(meaning):
+    """Формат сообщения на вывод"""
+    date = format_date(meaning.get('date'))
+    description = meaning.get('description')
+    from_ = mask_in_message(meaning.get('from_'))
+    from_ = '{from_} -> ' if from_ else ''
+    to = mask_in_message(meaning.get('to'))
+    amount = meaning.get('operationAmount').get('amount')
+    currency = meaning.get('operationAmount').get('currency').get('name')
+    return f'{date} {description}/n{from_}{to}/n{amount}{currency}'
